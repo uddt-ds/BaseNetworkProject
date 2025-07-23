99caad4e284d8c906373f2e5343bc7af156f393b
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-23 16:16:24 +0900

    [Feat] BaseViewController 구현

---
+++ BaseNetworkProject/Base/BaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseViewController.swift
+//  NetworkBaseProject
+//
+//  Created by Lee on 7/23/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierachy()
+        configureUI()
+        setConstraints()
+    }
+
+    func configureHierachy() {
+        
+    }
+
+    func configureUI() {
+
+    }
+
+    func setConstraints() {
+
+    }
+}
+
+//protocol BaseVCProtocol {
+//    func configureHierachy()
+//    func configureUI()
+//    func setConstraints()
+//}
